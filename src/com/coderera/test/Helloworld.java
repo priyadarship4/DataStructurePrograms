105a2a121d36e59a81de529178c2ca544bdf4a33
piyush <piyush@developer>
2017-10-19 13:33:58 +0530

    adding a addition module

--- src/com/coderera/test/Helloworld.java
+++ src/com/coderera/test/Helloworld.java
@@ -3,5 +3,8 @@ package com.coderera.test;
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("piyush");
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
 	}
 }
